ee9676e185a54042982ee4835d52e704439c0f75
Thomas Ferraz <thomasferraz@gmail.com>
2020-09-03 00:21:50 -0300

    First Commit

---
+++ GameTop.Lib/Jogador1.cs
@@ -0,0 +1,25 @@
+using GameTOP.Interface;
+
+namespace GameTop.Lib
+{
+ public class Jogador1 : iJogador
+    {
+        public readonly string _Nome;
+         public Jogador1(string nome = "Ronaldo")
+         {
+             _Nome = nome;
+         }
+        public string Chuta()
+        {
+            return $"{_Nome} está chutando  \n";
+        }
+        public string Corre()
+        {
+            return $"{_Nome} está Correndo  \n";
+        }
+        public string Passe()
+        {
+            return $"{_Nome} está Passando  \n";
+        }
+    }
+}
---
+++ GameTop.Lib/Jogador2.cs
@@ -0,0 +1,22 @@
+using GameTOP.Interface;
+
+namespace GameTop.Lib
+{
+    public class Jogador2 : iJogador
+    {
+        public string Chuta()
+        {
+            return "Maradora estas pateando \n";
+        }
+
+        public string Corre()
+        {
+            return "Maradora estas corriendo \n";
+        }
+
+        public string Passe()
+        {
+            return "Maradora estas pasando \n";
+        }
+    }
+}
